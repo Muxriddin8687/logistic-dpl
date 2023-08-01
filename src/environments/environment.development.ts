9d136c52726dbc898e3a33eb37df33caa07fda5a
muxriddin8687 <muxriddinsatipboyev@gmail.com>
2023-08-01 16:08:05 +0500

    add firebase tools

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,14 @@
 export const environment = {
     prod: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyDtQvfkROwE588a8AwxDtAbXr_gtQGRbrw",
+        authDomain: "diamond-pro-logistics.firebaseapp.com",
+        projectId: "diamond-pro-logistics",
+        storageBucket: "diamond-pro-logistics.appspot.com",
+        messagingSenderId: "159454798374",
+        appId: "1:159454798374:web:5c7f94fd7ab190033abce7",
+        measurementId: "G-C47LBNM3YR"
+    },
     companyApi: 'https://api.thecompaniesapi.com/v1/companies?size=10&token=XkjLsLUY&search=',
     telegram_api: 'https://api.telegram.org/bot5201252679:AAEqE5sjhIL94ajvc04nwDJIIVR3H2m30I8/sendMessage?chat_id=@',
 };
