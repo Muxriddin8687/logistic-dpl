b81fc5f4175880a30417892d27345206c89a2f10
muxriddin8687 <muxriddinsatipboyev@gmail.com>
2023-09-04 03:02:33 +0500

    add email message to contact us page

--- src/app/core/services/message.service.ts
+++ src/app/core/services/message.service.ts
@@ -18,7 +18,7 @@ export class MessageService {
 
   sendSms(data: any, chanel: string): Observable<any> {
     // return this._http.get<any>(`${this.telegram_api}${chanel}&text=${data}`, { 'headers': this.headers })
-    return this._http.get<any>(`${this.telegram_api}+aMh0bhF6Ug44YjRi&text=${data}`, { 'headers': this.headers })
+    return this._http.get<any>(`${this.telegram_api}aMh0bhF6Ug44YjRi&text=${data}`, { 'headers': this.headers })
       .pipe(
         debounceTime(5000),
         distinctUntilChanged()
